2ab075cd6682ea5d13649eda93f53a21dd564fba
Emanuel Pereira <51258876841@escola928.edu>
2025-05-06 11:23:23 -0300

    06/05/25

--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -1,4 +1,6 @@
 const mysql = require("../mysql");
+const bcrypt = require("bcrypt"); 
+const jwt = require("jsonwebtoken");
 
 exports.atualizarUsuario = async (req, res) => {
     try {
@@ -28,6 +30,7 @@ exports.atualizarUsuario = async (req, res) => {
 
 exports.cadastrarUsuario = async (req, res) => {
     try {
+        const hash = await bcrypt.hash(req.body.password, 10);
         const resultado = await mysql.execute(
         `INSERT INTO users (
             first_name,
@@ -40,7 +43,7 @@ exports.cadastrarUsuario = async (req, res) => {
                 req.body.first_name, 
                 req.body.last_name,
                 req.body.email, 
-                req.body.password,
+                hash,
                 req.body.birth_date,
                 req.body.phone
             ]);
@@ -53,5 +56,34 @@ exports.cadastrarUsuario = async (req, res) => {
     } catch (error) {
         return res.status(500).send({ "Mensagem": error });
 
+    }
+}
+
+exports.login = async (req, res) => {
+    try {
+        const usuario = await mysql.execute(
+            `SELECT * FROM users WHERE email = ?`,
+             [req.body.email])
+        if (usuario.length == 0) {
+            return res.status(401).send({ "Mensagem": "Usuario não cadastrado!" });
+        }
+        
+        const  match = await bcrypt.compare(usuario[0].password, req.body.password);
+        if (!match) {
+            return res.status(401).send({ "Mensagem": "Senha incorreta!" });
+        }
+
+
+        const token = jwt.sign({
+            id: usuario[0].id,
+            first_name: usuario[0].first_name,
+            last_name: usuario [0].last_name,
+            email: usuario[0].email,
+            birth_date: usuario[0].birth_date,
+        }, "senhadojwt")
+
+
+    } catch (error) {
+        return res.status(500).send({"Error":error});
     }
 }
